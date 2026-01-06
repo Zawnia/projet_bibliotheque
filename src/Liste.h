21c2075a3efcf8425fef674d51cdbf6787598f9b
Zawnia <al84250@gmail.com>
2026-01-06 11:44:50 +0100

    structure projet src diagrams et root

---
+++ src/Liste.h
@@ -0,0 +1,29 @@
+#ifndef LISTE_H
+#define LISTE_H
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Liste
+{
+};
+
+template <typename T>
+class Noeud
+{
+    friend class Liste<T>;
+
+private:
+    T info;
+    Noeud<T> *suiv;
+
+public:
+    Noeud(T donnee)
+    {
+        this->info = donnee;
+        this->suiv = NULL;
+    }
+};
+
+#endif
---
+++ src/livre.h
@@ -0,0 +1,50 @@
+#ifndef LIVRE_H
+#define LIVRE_H
+
+#include <iostream>
+using namespace::std;
+
+class Livre {
+
+private :
+    double code;
+    string auteur;
+    string editeur;
+    double isbn;
+    string publique;
+    string etat;
+
+public :
+    Livre();
+    ~Livre();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
