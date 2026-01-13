dd0681f2a2588f2625062b3acfb4962bf05c2972
Zawnia <al84250@gmail.com>
2026-01-13 15:01:18 +0100

    Livre qui marche normalment

--- src/livre.h
+++ src/livre.h
@@ -6,46 +6,25 @@ using namespace::std;
 
 class Livre {
 
-private :
+protected :
     double code;
     string auteur;
     string editeur;
-    double isbn;
-    string publique;
+    string isbn;
+    string public_cible;
     string etat;
 
 public :
+    Livre(double code, std::string auteur, std::string editeur, std::string isbn, std::string public_cible, std::string etat);
     Livre();
     ~Livre();
-    
+    void afficher();
+    string getISBN();
+    int getCode();
+    string getEtat();
+    void setEtat(string nouvel_etat); 
 };
 
+#endif 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#endif
